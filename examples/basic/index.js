93790a5275d2caeb4b2d60812aba863b37bd2b25
糖饼 <sugarpie.tang@gmail.com>
2020-12-09 12:15:49 +0800

    doc: upd index.html

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -3,5 +3,5 @@ style.textContent = `html { color: #003cb1 }`;
 document.head.appendChild(style);
 
 const hello = document.createElement('h1');
-hello.innerHTML = `hello world`;
+hello.innerHTML = `hello ☂️  world`;
 document.body.appendChild(hello);
