febd865452d21d03952b3d551a2917ed4d9957c4
糖饼 <sugarpie.tang@gmail.com>
2020-12-11 12:09:58 +0800

    fix: xss

--- examples/sandbox-security-error/index.js
+++ examples/sandbox-security-error/index.js
@@ -1,8 +1,30 @@
-try {
-    document.body.innerHTML = `<img onerror="alert('xss')" />`;
-} catch(error) {
-    document.body.innerHTML = error;
-    console.error(error);
-    debugger;
-}
+const testList = [
+  `<a href="javascript:alert('xss')">link</a>`,
+  `<a href=" javascript:alert('xss') ">link</a>`,
+  `<a HREF="javascript:alert('xss')">link</a>`,
+  `<a HREF="JavaScript:alert('xss')">link</a>`,
+  `<a HREF=" JavaScript:alert('xss') ">link</a>`,
+  `<img src="javascript:alert('xss')" />`,
+  `<form action="javascript:alert('xss')"></form>`,
+  `<img onerror="alert('xss')" />`,
+  `<img onError=" alert('xss') " />`,
+  `<iframe src="javascript:alert('xss')"></iframe>`,
+  `<iframe srcdoc="&lt;script&gt;alert('xss')&lt;/script&gt;"></iframe>`,
+  `<iframe SRCDOC="&lt;script&gt;alert('xss')&lt;/script&gt;"></iframe>`
+];
 
+testList.forEach((html, index) => {
+  html = html.replace(`'xss'`, `'${index}: xss'`);
+  const target = document.createElement('div');
+  const log = document.createElement('div');
+
+  document.body.appendChild(log);
+  document.body.appendChild(target);
+
+  try {
+    target.innerHTML = html;
+    log.textContent = `${index}: Successful: ${html}`;
+  } catch (error) {
+    log.textContent = `${index}: Error: ${error.message}`;
+  }
+});
