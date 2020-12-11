f32263fa7771db0fb98074fa4ba251d765b80ec7
糖饼 <sugarpie.tang@gmail.com>
2020-12-11 12:44:21 +0800

    upd

--- examples/sandbox-security-error/index.js
+++ examples/sandbox-security-error/index.js
@@ -1,20 +1,27 @@
 const testList = [
-  `<a href="javascript:alert('xss')">link</a>`,
-  `<a href=" javascript:alert('xss') ">link</a>`,
-  `<a HREF="javascript:alert('xss')">link</a>`,
-  `<a HREF="JavaScript:alert('xss')">link</a>`,
-  `<a HREF=" JavaScript:alert('xss') ">link</a>`,
-  `<img src="javascript:alert('xss')" />`,
-  `<form action="javascript:alert('xss')"></form>`,
-  `<img onerror="alert('xss')" />`,
-  `<img onError=" alert('xss') " />`,
-  `<iframe src="javascript:alert('xss')"></iframe>`,
-  `<iframe srcdoc="&lt;script&gt;alert('xss')&lt;/script&gt;"></iframe>`,
-  `<iframe SRCDOC="&lt;script&gt;alert('xss')&lt;/script&gt;"></iframe>`
+  `<script>alert('$xss')</script>`,
+  `<script type="text/javascript">alert('$xss')</script>`,
+  `<script type="text/ecmascript">alert('$xss')</script>`,
+  `<script type="application/javascript">alert('$xss')</script>`,
+  `<script type="module">alert('$xss')</script>`,
+  `<script type="text/html">alert('$xss')</script>`,
+  `<a href="javascript:alert('$xss')">link</a>`,
+  `<a href=" javascript:alert('$xss') ">link</a>`,
+  `<a HREF="javascript:alert('$xss')">link</a>`,
+  `<a HREF="JavaScript:alert('$xss')">link</a>`,
+  `<a HREF=" JavaScript:alert('$xss') ">link</a>`,
+  `<img src="javascript:alert('$xss')" />`,
+  `<form action="javascript:alert('$xss')"></form>`,
+  `<img onerror="alert('$xss')" />`,
+  `<img onError=" alert('$xss') " />`,
+  `<IMG ONERROR="alert('$xss')" />`,
+  `<iframe src="javascript:alert('$xss')"></iframe>`,
+  `<iframe srcdoc="&lt;script&gt;alert('$xss')&lt;/script&gt;"></iframe>`,
+  `<iframe SRCDOC="&lt;script&gt;alert('$xss')&lt;/script&gt;"></iframe>`
 ];
 
 testList.forEach((html, index) => {
-  html = html.replace(`'xss'`, `'${index}: xss'`);
+  html = html.replace(`$xss`, `${index}: xss`);
   const target = document.createElement('div');
   const log = document.createElement('div');
 
@@ -22,6 +29,7 @@ testList.forEach((html, index) => {
   document.body.appendChild(target);
 
   try {
+    target.dataset.test = index;
     target.innerHTML = html;
     log.textContent = `${index}: Successful: ${html}`;
   } catch (error) {
