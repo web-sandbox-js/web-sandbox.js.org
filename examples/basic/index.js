3eea0d193df4d515462dc045145c2d56d8bdd99c
糖饼 <sugarpie.tang@gmail.com>
2020-12-11 18:55:58 +0800

    upd doc

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,7 +1,3 @@
-const style = document.createElement('style');
-style.textContent = `html { color: #003cb1 }`;
-document.head.appendChild(style);
-
-const hello = document.createElement('h1');
+const hello = document.createElement('h3');
 hello.innerHTML = `hello ☂️  world`;
 document.body.appendChild(hello);
