4b6a5ac6eb6a2999a2a4c1cb4c05f48b41f30d27
糖饼 <sugarpie.tang@gmail.com>
2020-12-14 11:29:55 +0800

    upd

--- examples/single-spa/navbar/index.js
+++ examples/single-spa/navbar/index.js
@@ -1,37 +1,30 @@
-(function(global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined'
-    ? (module.exports = factory())
-    : typeof define === 'function' && define.amd
-    ? define(factory)
-    : ((global = global || self), (global.Realm = factory()));
-})(this, function() {
-  let style, template;
-  return {
-    bootstrap: function bootstrap(props) {
-      return Promise.resolve().then(() => {
-        style = document.createElement('style');
-        style.textContent = `nav { color: #000 }`;
-        template = document.createElement('nav');
-        template.innerHTML = `
-        <a href="#/home">home</a>
-        |
-        <a href="#/sandbox-window-api">sandbox-window-api</a>
-        |
-        <a href="#/vue-todomvc">vue-todomvc</a>
-        `;
-      });
-    },
-    mount: function mount(props) {
-      return Promise.resolve().then(() => {
-        document.head.appendChild(style);
-        document.body.appendChild(template);
-      });
-    },
-    unmount: function unmount(props) {
-      return Promise.resolve().then(() => {
-        document.head.removeChild(style);
-        document.body.removeChild(template);
-      });
-    }
-  };
-});
+let style, template;
+
+export function bootstrap(props) {
+  return Promise.resolve().then(() => {
+    style = document.createElement('style');
+    style.textContent = `nav { color: #000 }`;
+    template = document.createElement('nav');
+    template.innerHTML = `
+    <a href="#/home">home</a>
+    |
+    <a href="#/sandbox-window-api">sandbox-window-api</a>
+    |
+    <a href="#/vue-todomvc">vue-todomvc</a>
+    `;
+  });
+}
+
+export function mount(props) {
+  return Promise.resolve().then(() => {
+    document.head.appendChild(style);
+    document.body.appendChild(template);
+  });
+}
+
+export function unmount(props) {
+  return Promise.resolve().then(() => {
+    document.head.removeChild(style);
+    document.body.removeChild(template);
+  });
+}
