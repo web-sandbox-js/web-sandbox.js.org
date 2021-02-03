45036139fe3a20573a346433017581bebad70ad8
糖饼 <sugarpie.tang@gmail.com>
2021-02-04 00:43:56 +0800

    upd

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,3 +1,12 @@
-const hello = document.createElement('h3');
-hello.innerHTML = `hello ☂️  world`;
-document.body.appendChild(hello);
+const button = document.createElement('button');
+button.textContent = 'Click Me';
+button.addEventListener('click', () => {
+  const hello = document.createElement('p');
+  hello.innerHTML = `Hello WebSandbox`;
+  document.body.appendChild(hello);
+});
+document.body.appendChild(button);
+
+const style = document.createElement('style');
+style.textContent = `body { color: #003cb1 }`;
+document.head.appendChild(style);
