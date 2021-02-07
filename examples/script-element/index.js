3caa3c2b085983cdc30c4aec434476f233d70849
糖饼 <sugarpie.tang@gmail.com>
2021-02-07 18:19:14 +0800

    upd

---
+++ examples/script-element/index.js
@@ -0,0 +1,3 @@
+const script = document.createElement('script');
+script.src = '/examples/script-element/sub.js';
+document.body.appendChild(script);
---
+++ examples/script-element/sub.js
@@ -0,0 +1,2 @@
+document.body.innerHTML = `document.currentScript: ${document.currentScript.src}`;
+console.log('currentScript', document.currentScript);
