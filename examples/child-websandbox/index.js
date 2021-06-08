aecd67dca9035b674dcfb8132ef72cc92d287d1f
糖饼 <sugarpie.tang@gmail.com>
2021-06-08 19:47:50 +0800

    upd

--- examples/child-websandbox/index.js
+++ examples/child-websandbox/index.js
@@ -1,4 +1,25 @@
-const sandbox = document.createElement('web-sandbox');
-sandbox.name = 'sub-sandbox';
-sandbox.src = '/examples/vue-todomvc/index.js';
-document.body.appendChild(sandbox);
+document.body.innerHTML = `
+  <web-sandbox
+    src="/examples/vue-todomvc/index.js"
+    csp="
+      default-src 'none';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval';
+      style-src 'self' 'unsafe-inline';
+      navigate-to 'self' google.com web-sandbox.js.org;
+    ">
+  </web-sandbox>
+  <a href="https://google.com">google</a>
+  <a href="https://web-sandbox.js.org">web-sandbox</a>
+`;
+
+const sandbox = document.querySelector('web-sandbox');
+sandbox.evaluate(
+  (() => {
+    const nav = document.createElement('nav');
+    nav.innerHTML = `
+    <a href="https://google.com">google</a>
+    <a href="https://web-sandbox.js.org">web-sandbox</a>
+  `;
+    document.body.appendChild(nav);
+  }).toString()
+);
--- examples/child-websandbox/sub.js
+++
@@ -1,6 +0,0 @@
-const number = document.createElement('strong');
-document.body.appendChild(number);
-
-setInterval(() => {
-  number.textContent = Math.random();
-}, 500);
